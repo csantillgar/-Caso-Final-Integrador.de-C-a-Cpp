5a01e93160dd8ea5549bcba9a68510eba06f5906
csantillgar <csantill@myuax.com>
2023-11-30 12:05:19 +0100

    color consola

---
+++ Caso final intgrador/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+#include<cstdio>
+using namespace std;
+
+struct ColorConsole {
+    static constexpr auto fg_blue = "\033[34m";
+    static constexpr auto fg_white = "\033[47m";
+};
