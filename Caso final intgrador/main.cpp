cbb8b3d3bad55d9fafab1e51f237aafbf75998de
csantillgar <csantill@myuax.com>
2023-11-30 12:11:29 +0100

    prueba

--- Caso final intgrador/main.cpp
+++ Caso final intgrador/main.cpp
@@ -7,3 +7,52 @@ struct ColorConsole {
     static constexpr auto fg_blue = "\033[34m";
     static constexpr auto fg_white = "\033[47m";
 };
+
+ConsoleBox *consoleBox = new ConsoleBox;
+void load_script(const char* filename, bool show_script = false)
+{
+    string script;
+    FILE* f = nullptr;
+    try
+    {
+        f = fopen(filename, "rb");
+        if (!f)
+        {
+            cerr << "error de apertura de " << filename << endl;
+            return;
+        }
+
+        int c;
+        char buf[4001];
+        while ((c = fread(buf, 1, 4000, f)) > 0)
+        {
+            buf[c] = 0;
+            script.append(buf);
+        }
+        fclose(f);
+        f = nullptr;
+
+        if (show_script)
+        {
+            cout << ColorConsole::fg_blue << ColorConsole::bg_white;
+            cout << script << endl;
+        }
+        consoleBox->new_text();
+        consoleBox->set_text(script);
+    }
+    catch (...)
+    {
+        cerr << "error durante la lectura del archivo" << endl;
+        if(f)
+            fclose(f);
+    }
+}
+
+void load_script()
+{
+    char filename[500];
+    printf("Archivo: ");
+    scanf("%499s", filename);
+    load_script(filename, true);
+}
+}
